717742e50393b217ef3e7d876b49d059437cfb9e
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-02-21 21:38:52 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function (){
                 alert("you clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert("you clicked `${gameType}`");
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 
 })
 
@@ -22,11 +24,17 @@ document.addEventListener("DOMContentLoaded", function (){
  * and after the user's answer has been proccessed
  */
 
-function runGame() {
+function runGame(gameType) {
     // Create two random numbers between 1 and 25
 
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -47,8 +55,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubstractQuestion() {
