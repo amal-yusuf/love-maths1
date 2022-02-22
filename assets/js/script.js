d331ee37e4eefb73a4cf6105c1b4b589754311a7
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-02-22 10:00:35 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                checkAnswer(); 
-            } else {
+                checkAnswer();
+            }
+            else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
@@ -31,7 +32,9 @@ function runGame(gameType) {
     let num2 = Math.floor(Math.random() * 25) +1;
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply")
+    displayMultiplyQuestion(num1, num2);
+     else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -74,6 +77,9 @@ function calculateCorrectAnswer() {
 
     if ("operator" === "+") {
         return [operand1 + operand2, "addition"];
+    } else if ("operator" === "x") {
+        return [operand1 * operand2, "multiply"];
+
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting!`;
@@ -107,9 +113,14 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubstractQuestion() {
+    
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
